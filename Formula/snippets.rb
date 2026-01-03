9c6054572167c4d4ed68583e841c6e8a74ee450a
yuval-a <yuval.invoke@gmail.com>
2026-01-03 23:46:56 +0000

    chore: pin sha256 for macOS v1.0.0 binary

--- Formula/snippets.rb
+++ Formula/snippets.rb
@@ -6,7 +6,7 @@ class Snippets < Formula
   # Prebuilt binary (Apple Silicon). If you need Intel, we can add it in a follow-up.
   on_macos do
     url "https://github.com/yuval-a/snippets/releases/download/v1.0.0/snippets-v1.0.0-macos.tar.gz"
-    sha256 "REPLACE_WITH_SHA256"
+    sha256 "d79e65065a680835f4f94b9f8449fd8ac9c4ee59cf3a6e91383ba056f3902dbe"
   end
 
   def install
